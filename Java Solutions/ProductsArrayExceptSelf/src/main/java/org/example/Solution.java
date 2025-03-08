1eaae51e2ff8190ba16d747b14c3e4f61c97b837
yasuomaidana <yasuomaidana@gmail.com>
2025-03-08 12:52:33 -0600

    Solving ProductsArrayExceptSelf with Java

--- Java Solutions/ProductsArrayExceptSelf/src/main/java/org/example/Solution.java
+++ Java Solutions/ProductsArrayExceptSelf/src/main/java/org/example/Solution.java
@@ -2,6 +2,17 @@ package org.example;
 
 public class Solution {
     public int[] productExceptSelf(int[] nums) {
-        
+        int result[] = new int[nums.length];
+        int left_product = 1;
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = left_product;
+            left_product *= nums[i];
+        }
+        int right_product = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            result[i] *= right_product;
+            right_product *= nums[i];
+        }
+        return result;
     }
 }
