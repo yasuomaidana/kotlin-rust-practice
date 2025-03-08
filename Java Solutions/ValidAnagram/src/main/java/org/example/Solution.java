7a9db4cb05f4089c173af2450cb1e9ebd3c1111e
yasuomaidana <yasuomaidana@gmail.com>
2025-03-07 18:43:15 -0600

    Handling non equal length case

--- Java Solutions/ValidAnagram/src/main/java/org/example/Solution.java
+++ Java Solutions/ValidAnagram/src/main/java/org/example/Solution.java
@@ -4,6 +4,9 @@ import java.util.HashMap;
 
 public class Solution {
     public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
         HashMap<Character, Integer> map = new HashMap<>();
         for (char c : s.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
