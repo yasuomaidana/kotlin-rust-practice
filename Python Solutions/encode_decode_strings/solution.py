6a63a976794253f49b3815585d5ceeb309f0f789
yasuomaidana <yasuomaidana@gmail.com>
2025-03-07 23:18:22 -0600

    Solving decoder v1

--- Python Solutions/encode_decode_strings/solution.py
+++ Python Solutions/encode_decode_strings/solution.py
@@ -1,10 +1,25 @@
 from typing import List
 
 
-class Solution:
+def encode(strs: List[str]) -> str:
+    return "".join(f"{len(s)}:{s}" for s in strs )
 
-    def encode(self, strs: List[str]) -> str:
-        pass
+def decode(s: str) -> List[str]:
+    word_length = -1
+    length_start = 0
+    words = []
+    for i in range(len(s)):
+        if word_length <= 0 and s[i] == ":":            
+            word_length = int(s[length_start:i])
+            length_start = i + 1
+        elif word_length == 0:
+            words.append(s[length_start:i])
+            length_start = i
+        elif word_length > 0:
+            word_length -= 1
+    return words
 
-    def decode(self, s: str) -> List[str]:
-        pass
+if __name__ == "__main__":
+    encoded = encode(["neet","code","love","you"])
+    print(encoded)
+    print(decode(encoded))
