479e15b7c9bc5c3597eb4e7ef88222f852ac1f83
yasuomaidana <yasuomaidana@gmail.com>
2025-03-07 20:29:54 -0600

    Starting Encode and Decode Strings problem

---
+++ Java Solutions/EncodeDecodeStrings/src/main/java/org/example/Solution.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.List;
+
+public class Solution {
+    public String encode(List<String> strs) {
+        return null;
+    }
+
+    public List<String> decode(String str) {
+        return null;
+    }
+}
