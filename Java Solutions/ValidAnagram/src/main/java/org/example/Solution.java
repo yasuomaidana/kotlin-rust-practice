358d194de28c48d8cc8d78c4830cd7ec0c072481
yasuomaidana <yasuomaidana@gmail.com>
2025-03-07 18:25:18 -0600

    Solving is anagram with Java

---
+++ Java Solutions/ValidAnagram/src/main/java/org/example/Solution.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.HashMap;
+
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (char c : t.toCharArray()) {
+            if (!map.containsKey(c)) {
+                return false;
+            }
+            map.put(c, map.get(c) - 1);
+            if (map.get(c) == 0) {
+                map.remove(c);
+            }
+        }
+        return map.isEmpty();
+    }
+}
