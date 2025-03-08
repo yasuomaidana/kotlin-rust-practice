022570c3809ef95266ea008a266b0c5931d28ab8
yasuomaidana <yasuomaidana@gmail.com>
2025-03-07 20:11:11 -0600

    Solving Top K Elements Java

--- Java Solutions/TopKFrequentElements/src/main/java/org/example/Solution.java
+++ Java Solutions/TopKFrequentElements/src/main/java/org/example/Solution.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.util.HashMap;
+
 public class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        return null;
+        HashMap<Integer, Integer> frequency = new HashMap<>();
+
+        for (int num : nums) {
+            frequency.put(num, frequency.getOrDefault(num, 0) + 1);
+        }
+
+        return frequency.entrySet()
+                .stream()
+                .sorted((entry1,
+                         entry2) -> entry2.getValue() - entry1.getValue())
+                .limit(k)
+                .map(entry -> entry.getKey())
+                .mapToInt(i -> i)
+                .toArray();
     }
 }
