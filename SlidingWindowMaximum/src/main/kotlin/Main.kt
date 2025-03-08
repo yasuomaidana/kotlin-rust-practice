784cc289aab25f3a691762f18b6f30a8d0d4f408
yasuomaidana <yasuomaidana@gmail.com>
2025-03-07 18:16:33 -0600

    Starting Python Solutions

--- SlidingWindowMaximum/src/main/kotlin/Main.kt
+++ SlidingWindowMaximum/src/main/kotlin/Main.kt
@@ -1,16 +1,37 @@
 package org.example
 
+import java.util.ArrayDeque
+
+fun maxSlidingWindow(nums: IntArray, k: Int): IntArray {
+    val output = mutableListOf<Int>()
+    val q = ArrayDeque<Int>()
+    var l = 0
+
+    for (r in nums.indices) {
+        while (q.isNotEmpty() && nums[q.last()] < nums[r]) {
+            q.removeLast()
+        }
+        q.addLast(r)
+        if (l > q.first()) {
+            q.removeFirst()
+        }
+        if ((r + 1) >= k) {
+            output.add(nums[q.first()])
+            l += 1
+        }
+    }
+    
+    return output.toIntArray()
+}
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
+
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    val k = 3
+    val nums = intArrayOf(1, 3, 5, 7, 6, 4, 3, 9)
+    val result = maxSlidingWindow(nums, k)
+    println(result.contentToString())
 }
