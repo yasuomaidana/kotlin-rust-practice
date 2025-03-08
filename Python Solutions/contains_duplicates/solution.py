4865609edfbb5a5be5ab95c6937d9908e563645c
yasuomaidana <yasuomaidana@gmail.com>
2025-03-07 18:25:56 -0600

    Following Python conventions

--- Python Solutions/contains_duplicates/solution.py
+++ Python Solutions/contains_duplicates/solution.py
@@ -1,12 +1,11 @@
 from typing import List
 
 
-class Solution:
-    def has_duplicates(self, nums: List[int]) -> bool:
-        duplicates = set()
-        for num in nums:
-            if num in duplicates:
-                return True
-            duplicates.add(num)
-        else:
-            return False
+def has_duplicates(nums: List[int]) -> bool:
+    duplicates = set()
+    for num in nums:
+        if num in duplicates:
+            return True
+        duplicates.add(num)
+    else:
+        return False
